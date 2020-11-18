86f717b1a45a950b608173e791629a4a04ad4793
Shadow-Flash <varung848@gmail.com>
2020-11-18 20:46:01 +0530

    Netflix Done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// 9b1f5c35159d85c710a712c11af8074f
+// https://api.themoviedb.org/3/movie/550?api_key=9b1f5c35159d85c710a712c11af8074f
+import logo from './logo.svg';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from "./Banner";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>
+  );
+}
+
+export default App;
+ 
